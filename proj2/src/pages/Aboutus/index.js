a798e6e677b652a63c1c90e05d4c29e4a4354dbc
Pavan63021 <kasupavankumar0604@gmail.com>
2023-03-29 11:40:17 +0530

    Navbar added

--- proj2/src/pages/Aboutus/index.js
+++ proj2/src/pages/Aboutus/index.js
@@ -1,7 +1,14 @@
+import { Typography } from '@mui/material';
+
 export default function Aboutus(){
     return(
         <>
-        my name is
+<Typography>Name: PAVAN KUMAR KASU</Typography>
+<Typography>Father Name: ASHALU</Typography>
+<Typography>Mother Name: LAXMI</Typography>
+<Typography>Email: kasupavankumar0604@gmail.com</Typography>
+<Typography>Phone No.: 6302177682</Typography>
         </>
+        
     )
-}
+    }
