c3b7600b670336fc7179a15903af82e0eb64dab4
Pavan63021 <kasupavankumar0604@gmail.com>
2023-03-31 11:55:02 +0530

    Profile added

--- proj2/src/components/Navbar/index.js
+++ proj2/src/components/Navbar/index.js
@@ -14,6 +14,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import { Link } from 'react-router-dom';
+import Pavan from '../../assets/Pavan.jpg'
 
 const pages = ['Aboutus','Skills','Hobbies','Qualifications'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -117,7 +118,7 @@ function Navbar() {
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
-              <Link to = {page} style = {{textDecorationColor : 'None'}}>  
+              <Link to = {page} style = {{textDecorationColor : 'arrow'}}>  
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
@@ -133,7 +134,7 @@ function Navbar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Pavan" src={Pavan} />
               </IconButton>
             </Tooltip>
             <Menu
